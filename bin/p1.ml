44cc77c3b8e18fc1e53203832734877cc09203ce
Lloyd <lloyd.williams1062@gmail.com>
2025-12-02 16:32:10 +1100

    🙃

--- bin/p1.ml
+++ bin/p1.ml
@@ -1,6 +1,6 @@
 type line = { left : bool; count : int64 }
 
-let counts = Array.make 99 0
+let counts = Array.make 100 0
 
 let rec read_lines fd arr =
   try
@@ -16,29 +16,24 @@ let rec read_lines fd arr =
   with _ -> close_in_noerr fd
 
 let calc_left acc count =
-  let mod_count = Int64.rem count 99L in
-  if Int64.sub acc mod_count < 0L then Int64.add (Int64.sub acc mod_count) 99L
-  else Int64.sub acc mod_count
+  let v = Int64.rem (Int64.sub acc count) 100L in
+  if v < 0L then Int64.add v 100L else v
 
-let calc_right acc count =
-  let mod_count = Int64.rem count 99L in
-  if Int64.add acc mod_count > 99L then Int64.sub (Int64.add acc mod_count) 99L
-  else Int64.add acc mod_count
+let calc_right acc count = Int64.rem (Int64.add acc count) 100L
 
-let rec main lines curr (acc : int64) =
-  if curr = Dynarray.length lines - 1 then print_string (Int64.to_string acc)
+let rec main lines curr (acc : int64) l r =
+  if curr = Dynarray.length lines then Printf.printf "%d\n" counts.(0)
   else
     let line = Dynarray.get lines curr in
-    let new_acc =
-      if line.left then calc_left acc line.count else calc_right acc line.count
-    in
-    let idx = (Int64.to_int new_acc mod 99) - 1 in
-    print_int idx;
+    let new_acc = if line.left then l acc line.count else r acc line.count in
+    let idx = Int64.to_int new_acc in
     counts.(idx) <- counts.(idx) + 1;
-    main lines (curr + 1) new_acc
+    main lines (curr + 1) new_acc l r
 
 let () =
   let fd = open_in "in/1" in
   let lines = Dynarray.create () in
   read_lines fd lines;
-  main lines 0 50L
+  main lines 0 50L calc_left calc_right;
+  Array.fill counts 0 (Array.length counts) 0;
+  main lines 0 50L calc_left2 calc_right2
